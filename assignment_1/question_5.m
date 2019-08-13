59f25d3f46b339e5762d6c151250780f2268ac6c
12019269 <12019269@LAB11B14V1006.adsroot.uts.edu.au>
2019-08-13 13:56:09 +1000

    build up a bit

--- assignment_1/question_5.m
+++ assignment_1/question_5.m
@@ -1,12 +1,16 @@
 clc, clear, close
 syms theta L k P lambda
 TPE = 1/2*k*(1/2*L*theta)^2 - P*(1/2*L*theta^2)==0
+
 TPE = TPE / theta^2
 TPE = expand(TPE)
 TPE = TPE / k
 TPE = expand(TPE)
-TPE = subs(TPE, (P*L)/(2*k),lambda*L/2)
+TPE = subs(TPE, P/k,lambda)
 A = lhs(TPE)
 A = eig(A)
-A = subs(A,lambda, P/k)
-P = solve(A,P)
+lambda = solve(A,lambda)
+clear syms
+syms L k P
+eqn = P/k == lambda
+P = solve(eqn,P)
